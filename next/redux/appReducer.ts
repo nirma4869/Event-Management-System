43be14544ee93fc3ad610c238ec6aba04695335c
nirma4869 <96113989+nirma4869@users.noreply.github.com>
2024-08-13 21:15:27 +0200

    Delete next/redux directory

--- next/redux/appReducer.ts
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from "redux";
-import event from "./slices/event";
-import user from "./slices/user";
-
-// ----------------------------------------------------------------------
-
-const appReducer = combineReducers({ event, user });
-
-export { appReducer };
