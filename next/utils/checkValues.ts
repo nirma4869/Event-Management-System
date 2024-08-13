c9c0c62525e61c4d287655b1c6bdd6b4268723ad
nirma4869 <96113989+nirma4869@users.noreply.github.com>
2024-08-13 21:15:52 +0200

    Delete next/utils directory

--- next/utils/checkValues.ts
+++
@@ -1,7 +0,0 @@
-export const isEmptyString = (value: string | undefined | null) => {
-  return !value || value === "" ? null : value;
-};
-
-export const isNullValue = (value: string | undefined | null) => {
-  return !value ? "" : value;
-};
--- next/utils/formatAddress.ts
+++
@@ -1,8 +0,0 @@
-import { Address } from "@/models/address";
-
-// ----------------------------------------------------------------------
-
-export function formatAddress(address: Address) {
-  const { line1, zip, city, country } = address;
-  return `${line1}, ${zip}, ${city}, ${country}`;
-}
